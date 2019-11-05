7400abc7409b4b92d35ff55d1ea0de4888586f36
Roland Walker <walker@pobox.com>
2019-11-05 09:35:49 -0500

    don't try to go back to prev move from header
    there is no previous move.
    
    this code is a remnant. the first version of these defuns allowed
    next/prev move to traverse between games.

--- pgn-mode.el
+++ pgn-mode.el
@@ -480,7 +480,7 @@ With numeric prefix ARG, move ARG moves backward."
           (thumb (point)))
       (when (or (looking-at "[^\n]*\\]")
                 (and (looking-at "\\s-*$") (looking-back "\\]\\s-*" 10)))
-        (re-search-backward "\n\n" nil t))
+        (error "No more moves."))
       (dotimes (counter arg)
         (when (pgn-mode-looking-at-legal-move)
           (setq thumb (point))
