80a25276dc628075f3c77c8eacdf6bb9d7e47494
Roland Walker <walker@pobox.com>
2019-11-14 14:25:20 -0500

    compare positions using "="

--- pgn-mode.el
+++ pgn-mode.el
@@ -522,7 +522,7 @@ With numeric prefix ARG, advance ARG moves forward."
         (skip-chars-forward "0-9.…\s-")
         (unless (pgn-mode-looking-at-legal-move)
           (goto-char thumb)
-          (when (eq thumb start)
+          (when (= thumb start)
             (error "No more moves.")))))))
 
 (defun pgn-mode-previous-move (arg)
@@ -562,7 +562,7 @@ With numeric prefix ARG, move ARG moves backward."
                (forward-sexp -1)))))
         (unless (pgn-mode-looking-at-legal-move)
           (goto-char thumb)
-          (when (eq thumb start)
+          (when (= thumb start)
             (error "No more moves.")))))))
 
 (defun pgn-mode-select-game ()
