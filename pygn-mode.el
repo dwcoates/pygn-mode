281a42fcb401b1f5fcbb75c5b3433cf2d2ec4492
Roland Walker <walker@pobox.com>
2019-11-19 11:07:14 -0500

    hide the cursor in the image-display buffer

--- pygn-mode.el
+++ pygn-mode.el
@@ -789,6 +789,7 @@ When called non-interactively, display the board corresponding to POS."
          (buf (get-buffer-create " *pygn-mode-board*"))
          (win (get-buffer-window buf)))
     (with-current-buffer buf
+      (setq cursor-type nil)
       (delete-region (point-min) (point-max))
       (insert-image (create-image svg-data 'svg t)))
     (display-buffer buf '(display-buffer-reuse-window))
